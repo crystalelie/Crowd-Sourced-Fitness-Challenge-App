0374325f91bb5ebf7b8126ee80822792c62e1bb4
jo-sean <joseanmanuel@gmail.com>
2022-07-20 17:08:36 -0700

    Minor goal_convert bug fix

--- goal_convert.py
+++ goal_convert.py
@@ -5,10 +5,16 @@ def goal_convert(content, goal_list):
     del content["goals[]"]
     content['exercise_type'] = content["exercise_type"].lower()
 
-    goal_string = ", ".join(goal_list)
+    goal_string = goal_list[0]
 
-    if goal_string[-2:] == ", ":
-        goal_string = goal_string[:-2]
+    while "" in goal_list:
+        goal_list.remove("")
+
+    # Only when there is more than one goal will commas be added.
+    if len(goal_list) > 1:
+        goal_string = ", ".join(goal_list)
+        if goal_string[-2:] == ", ":
+            goal_string = goal_string[:-2]
 
     content['goals'] = goal_string
 
